6fe6c0d8c5e7741ad328915774f499ff98a931ec
mayanksinghrathore <mayanksinghrathore818@gmail.com>
2021-06-15 13:28:19 +0530

    new changes in ui and functionality
    new changes in ui and functionality

--- Frontend/Day 5/script.js
+++ Frontend/Day 5/script.js
@@ -1,43 +1,75 @@
-const calculateAdd = () => {
-  const num1 = document.getElementById("number1").value;
-  const num2 = document.getElementById("number2").value;
-
-  const add = parseInt(num1) + parseInt(num2);
-  const subs = parseInt(num1) - parseInt(num2);
-  const multi = parseInt(num1) * parseInt(num2);
-  const divide = parseInt(num1) / parseInt(num2);
-
-  document.querySelector("#answer").innerHTML = add;
-};
-
-const calculateSubs = () => {
-  const num1 = document.getElementById("number1").value;
-  const num2 = document.getElementById("number2").value;
-
-  const subs = parseInt(num1) - parseInt(num2);
-
-  document.querySelector("#answer").innerHTML = subs;
-};
-
-const calculateMulti = () => {
-  const num1 = document.getElementById("number1").value;
-  const num2 = document.getElementById("number2").value;
-
-  const multi = parseInt(num1) * parseInt(num2);
-
-  document.querySelector("#answer").innerHTML = multi;
-};
-
-const calculateDivide = () => {
-  const num1 = document.getElementById("number1").value;
-  const num2 = document.getElementById("number2").value;
-
-  const divide = parseInt(num1) / parseInt(num2);
-
-  document.querySelector("#answer").innerHTML = divide;
-};
-
-document.querySelector("#add").addEventListener("click", calculateAdd);
-document.querySelector("#subs").addEventListener("click", calculateSubs);
-document.querySelector("#multi").addEventListener("click", calculateMulti);
-document.querySelector("#divide").addEventListener("click", calculateDivide);
+function getHistory() {
+  return document.getElementById('history-value').innerText;
+}
+function printHistory(num) {
+  document.getElementById('history-value').innerText = num;
+}
+function getOutput() {
+  return document.getElementById('output-value').innerText;
+}
+function printOutput(num) {
+  if (num == '') {
+    document.getElementById('output-value').innerText = num;
+  } else {
+    document.getElementById('output-value').innerText = getFormattedNumber(num);
+  }
+}
+function getFormattedNumber(num) {
+  if (num == '-') {
+    return '';
+  }
+  var n = Number(num);
+  var value = n.toLocaleString('en');
+  return value;
+}
+function reverseNumberFormat(num) {
+  return Number(num.replace(/,/g, ''));
+}
+var operator = document.getElementsByClassName('operator');
+for (var i = 0; i < operator.length; i++) {
+  operator[i].addEventListener('click', function() {
+    if (this.id == 'clear') {
+      printHistory('');
+      printOutput('');
+    } else if (this.id == 'backspace') {
+      var output = reverseNumberFormat(getOutput()).toString();
+      if (output) {
+        //if output has a value
+        output = output.substr(0, output.length - 1);
+        printOutput(output);
+      }
+    } else {
+      var output = getOutput();
+      var history = getHistory();
+      if (output == '' && history != '') {
+        if (isNaN(history[history.length - 1])) {
+          history = history.substr(0, history.length - 1);
+        }
+      }
+      if (output != '' || history != '') {
+        output = output == '' ? output : reverseNumberFormat(output);
+        history = history + output;
+        if (this.id == '=') {
+          var result = eval(history);
+          printOutput(result);
+          printHistory('');
+        } else {
+          history = history + this.id;
+          printHistory(history);
+          printOutput('');
+        }
+      }
+    }
+  });
+}
+var number = document.getElementsByClassName('number');
+for (var i = 0; i < number.length; i++) {
+  number[i].addEventListener('click', function() {
+    var output = reverseNumberFormat(getOutput());
+    if (output != NaN) {
+      //if output is a number
+      output = output + this.id;
+      printOutput(output);
+    }
+  });
+}
