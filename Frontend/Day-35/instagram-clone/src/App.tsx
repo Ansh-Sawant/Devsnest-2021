4f78a38ba4815341c422e4ce11da5ebd39001908
sawantansh17 <sawantansh17@gmail.com>
2021-10-15 16:00:13 +0530

    Instagram clone done

--- Frontend/Day-35/instagram-clone/src/App.tsx
+++ Frontend/Day-35/instagram-clone/src/App.tsx
@@ -1,11 +1,37 @@
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+import { useDispatch, useSelector } from "react-redux";
+import { User, updateUser } from "./actions";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./App.css";
 
+export interface PostType {
+  username: string;
+  post: string;
+}
+
 const App = () => {
+  const user = useSelector((state: any) => state.user);
+  const dispatch = useDispatch();
+  const [posts, setPosts] = React.useState<PostType[]>([]);
+  useEffect(() => {
+    dispatch(updateUser());
+    fetch("/data/post.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }, []);
+
   return (
-    <>
-      <div>Instagram</div>
-    </>
+    <div className="App">
+      <Navbar />
+      <Landing posts={posts} />
+    </div>
   );
 };
 
 export default App;
+// Ansh Sawant
